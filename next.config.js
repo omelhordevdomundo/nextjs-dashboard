27dad5751d05af1a8f72db122a1a43683c5ce9c1
Filipe Marques <omelhordevdomundo@gmail.com>
2025-02-15 04:25:38 -0300

    isso ai

--- next.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  assetPrefix:
-    process.env.NODE_ENV === "production" ? "/nextjs-dashboard/" : "",
-  basePath: process.env.NODE_ENV === "production" ? "/nextjs-dashboard" : "",
-  // ...existing code...
-};
